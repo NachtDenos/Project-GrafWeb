af40663332236255b887c3175f649a80a521cd52
Edson Argüello <eeatienda2001@hotmail.com>
2024-05-18 13:29:55 -0600

    Api Graph Facebook

--- GameHTML/gameEnded.php
+++ GameHTML/gameEnded.php
@@ -3,12 +3,11 @@ include '../conexion.php';
 
 session_start();
 
-if(isset($_SESSION['username'])) {
+if (isset($_SESSION['username'])) {
     $username = $_SESSION['username'];
     $user_id = $_SESSION['user_id'];
 
-    $user_id = $_SESSION['user_id'];
-    $sql = "SELECT pts FROM jugadores WHERE id = $user_id ";
+    $sql = "SELECT pts FROM jugadores WHERE id = $user_id";
 
     try {
         $result = $conn->query($sql);
@@ -27,11 +26,11 @@ if(isset($_SESSION['username'])) {
         die("Se produjo un error: " . $e->getMessage());
     }
 
-    if(isset($_GET['score'])) {
+    if (isset($_GET['score'])) {
         $new_score = intval($_GET['score']);
 
         // Comparar y actualizar si el nuevo puntaje es mayor
-        if($new_score > $current_pts) {
+        if ($new_score > $current_pts) {
             $update_sql = "UPDATE jugadores SET pts = $new_score WHERE id = $user_id";
 
             try {
@@ -45,7 +44,7 @@ if(isset($_SESSION['username'])) {
     }
 
     $conn->close();
-} 
+}
 ?>
 
 <!DOCTYPE html>
@@ -62,15 +61,13 @@ if(isset($_SESSION['username'])) {
             <img src="../GameImages/Logo.png">
         </div>
         <div class="leaderboard-wrapper">
-            <h1 id="scoreText" ></h1>
+            <h1 id="scoreText"></h1>
             <div class="leaderboard-image">
                 <img src="../GameImages/Pantalla juego/end.png">
             </div>
         </div>
-        <button class="button-share">
-                <a href="">
-                <img src="../GameImages/Pantalla juego/compartir.png" alt="Compartir en Facebook">
-                </a>
+        <button class="button-share" onclick="shareScore()">
+            <img src="../GameImages/Pantalla juego/compartir.png" alt="Compartir en Facebook">
         </button>
         <button class="back-button">
             <a href="../menu.php">
@@ -84,7 +81,43 @@ if(isset($_SESSION['username'])) {
             let scoreText = document.getElementById('scoreText');
             scoreText.innerText = score; 
 
+            function shareScore() {
+                const message = `Tu puntuación es: ${score}`;
+
+                FB.api(
+                    "/331396916715898/feed",
+                    "POST",
+                    {
+                        message: message,
+                        access_token: "EAANNCB2g0TgBO15hwQwnJnYrp0dmjC9hZCPENGDTT0HC580Q5nTFN59NOBVVI91HkDJYLEGJLASjrt5r5jAk6AZCacJhrhsaUl7XpZCh2UjJiOjKMWZCmXHFbTtRT3oRgDUibkN6Yg4d3rWxY8ZBqDIwAso3oMkdWGq4sOU6Okzk5Aeb3zxtTW8Q74tZCt8IZAdAZCO1BXiszPyOVZAGZBLsFhG1xE0fAdIAELzQZDZD",
+                    },
+                    function(response) {
+                        if (response && !response.error) {
+                            console.log("POST", response);
+                        } else {
+                            console.error("error");
+                        }
+                    }
+                );
+            }
+
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "https://connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+
+            window.fbAsyncInit = function() {
+                FB.init({
+                    appId: '1172744637406338',
+                    xfbml: true,
+                    version: 'v19.0'
+                });
+            };
         </script>
     </div>
 </body>
 </html>
+
